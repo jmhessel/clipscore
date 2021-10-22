459f821429a6a86feb22cef3a12fc2c634d6e093
Jack Hessel <jackh@allenai.org>
2021-10-21 23:40:42 -0700

    starter

---
+++ clipscore.py
@@ -0,0 +1,176 @@
+'''
+Code for CLIPScore (https://arxiv.org/abs/2104.08718)
+@inproceedings{hessel2021clipscore,
+  title={{CLIPScore:} A Reference-free Evaluation Metric for Image Captioning},
+  author={Hessel, Jack and Holtzman, Ari and Forbes, Maxwell and Bras, Ronan Le and Choi, Yejin},
+  booktitle={EMNLP},
+  year={2021}
+}
+'''
+import argparse
+import clip
+import torch
+from PIL import Image
+from sklearn.preprocessing import normalize
+from torchvision.transforms import Compose, Resize, CenterCrop, ToTensor, Normalize
+import torch
+import tqdm
+import numpy as np
+import sklearn.preprocessing
+import collections
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        'candidates_json',
+        type=str,
+        help='Candidates json mapping from image_id --> candidate.')
+        
+    parser.add_argument(
+        'image_dir',
+        type=str,
+        help='Directory of images, with the filenames as image ids.')
+        
+    parser.add_argument(
+        '--references_json',
+        default=None,
+        help='Optional references json mapping from image_id --> [list of references]')
+
+    return parser.parse_args()
+
+
+class CLIPCapDataset(torch.utils.data.Dataset):
+    def __init__(self, data, prefix='A photo depicts'):
+        self.data = data
+        self.prefix = prefix
+        if self.prefix[-1] != ' ':
+            self.prefix += ' '
+
+    def __getitem__(self, idx):
+        c_data = self.data[idx]
+        c_data = clip.tokenize(self.prefix + c_data, truncate=True).squeeze()
+        return {'caption': c_data}
+
+    def __len__(self):
+        return len(self.data)
+
+
+class CLIPImageDataset(torch.utils.data.Dataset):
+    def __init__(self, data):
+        self.data = data
+        # only 224x224 ViT-B/32 supported for now
+        self.preprocess = self._transform_test(224)
+
+    def _transform_test(self, n_px):
+        return Compose([
+            Resize(n_px, interpolation=Image.BICUBIC),
+            CenterCrop(n_px),
+            lambda image: image.convert("RGB"),
+            ToTensor(),
+            Normalize((0.48145466, 0.4578275, 0.40821073), (0.26862954, 0.26130258, 0.27577711)),
+        ])
+
+    def __getitem__(self, idx):
+        c_data = self.data[idx]
+        image = Image.open(c_data)
+        image = self.preprocess(image)
+        return {'image':image}
+
+    def __len__(self):
+        return len(self.data)
+
+
+def extract_all_captions(captions, model, device, batch_size=256, num_workers=8):
+    data = torch.utils.data.DataLoader(
+        CLIPCapDataset(captions),
+        batch_size=batch_size, num_workers=num_workers, shuffle=False)
+    all_text_features = []
+    with torch.no_grad():
+        for b in tqdm.tqdm(data):
+            b = b['caption'].to(device)
+            all_text_features.append(model.encode_text(b).cpu().numpy())
+    all_text_features = np.vstack(all_text_features)
+    return all_text_features
+
+
+def extract_all_images(images, model, device, batch_size=64, num_workers=8):
+    data = torch.utils.data.DataLoader(
+        CLIPImageDataset(images),
+        batch_size=batch_size, num_workers=num_workers, shuffle=False)
+    all_image_features = []
+    with torch.no_grad():
+        for b in tqdm.tqdm(data):
+            b = b['image'].to(device)
+            all_image_features.append(model.encode_image(b).cpu().numpy())
+    all_image_features = np.vstack(all_image_features)
+    return all_image_features
+    
+
+def get_clip_score(model, images, candidates, device, w=2.5):
+    '''
+    get standard image-text clipscore.
+    images can either be:
+    - a list of strings specifying filepaths for images
+    - a precomputed, ordered matrix of image features
+    '''
+    if isinstance(images, list):
+        # need to extract image features
+        images = extract_all_images(images, model, device)
+
+    candidates = extract_all_captions(candidates, model, device)
+
+    images = sklearn.preprocessing.normalize(images, axis=1)
+    candidates = sklearn.preprocessing.normalize(candidates, axis=1)
+
+    per = w*np.clip(np.sum(images * candidates, axis=1), 0, None)
+    return np.mean(per), per, candidates
+
+
+def get_refonlyclipscore(model, references, candidates, device):
+    '''
+    The text only side for refclipscore
+    '''
+    if isinstance(candidates, list):
+        candidates = extract_all_captions(candidates, model, device)
+
+    flattened_refs = []
+    flattened_refs_idxs = []
+    for idx, refs in enumerate(references):
+        flattened_refs.extend(refs)
+        flattened_refs_idxs.extend([idx for _ in refs])
+
+    flattened_refs = extract_all_captions(flattened_refs, model, device)
+
+    candidates = sklearn.preprocessing.normalize(candidates, axis=1)
+    flattened_refs = sklearn.preprocessing.normalize(flattened_refs, axis=1)
+
+    cand_idx2refs = collections.defaultdict(list)
+    for ref_feats, cand_idx in zip(flattened_refs, flattened_refs_idxs):
+        cand_idx2refs[cand_idx].append(ref_feats)
+
+    assert len(cand_idx2refs) == len(candidates)
+
+    cand_idx2refs = {k: np.vstack(v) for k, v in cand_idx2refs.items()}
+
+    per = []
+    for c_idx, cand in tqdm.tqdm(enumerate(candidates)):
+        cur_refs = cand_idx2refs[c_idx]
+        all_sims = cand.dot(cur_refs.transpose())
+        per.append(np.max(all_sims))
+
+    return np.mean(per), per
+
+
+def main():
+    args = parse_args()
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    model, transform = clip.load("ViT-B/32", device=device, jit=False)
+    model.eval()
+
+    image_paths = [os.path.join(args.image_dir, path) for path in os.listdir(args.image_dir)
+                   if path.endswith(('.png', '.jpg', '.jpeg', '.tiff'))]
+    print(image_paths)
+    
+if __name__ == '__main__':
+    main()
